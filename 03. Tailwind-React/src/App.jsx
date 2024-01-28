219c08ebe02d4387bb2cb557e3b2d72d60a98238
darshan72 <darshankhairnar72@gmail.com>
2024-01-28 15:39:41 +0530

    implemented tailwind in react

---
+++ 03. Tailwind-React/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <div className="text-red-700">
+        <h1>This is tailwind from react</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
